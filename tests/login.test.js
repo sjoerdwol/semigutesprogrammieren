f0517bdfa8a04bb81a40c3ac68de9b3a04b94a43
leuthold.jannik <leuthold.jannik@student.dhbw-karlsruhe.de>
2021-05-29 12:35:37 +0200

    sprint 1 - BLI 1 - initialized express server and refined structure

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,5 @@
-const login = require('../main/js/login.js')
-const loginOnClick = require('../main/js/login.js')
+const login = require('../public/javascripts/login.js')
+const loginOnClick = require('../public/javascripts/login.js')
 
 test('return false when username empty string', () => {
     expect(login("", "testPW")).toBe(false)
