b5118965856c759564ef6f5e73d679d38013eedb
leuthold.jannik <leuthold.jannik@student.dhbw-karlsruhe.de>
2021-05-29 14:40:40 +0200

    sprint 1 - BL 1 - database initialization, part 2

--- database.js
+++ database.js
@@ -3,5 +3,6 @@ var admin = require("firebase-admin");
 var serviceAccount = require("secret/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://semigutesprogrammieren.firebaseio.com'
 });
