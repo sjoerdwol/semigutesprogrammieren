5df2ad4b00541e7f149be6d820a817a324fcf8bf
leuthold.jannik <leuthold.jannik@student.dhbw-karlsruhe.de>
2021-05-29 14:35:18 +0200

    sprint 1 - BLI 1 - database initialization

---
+++ database.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("secret/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
