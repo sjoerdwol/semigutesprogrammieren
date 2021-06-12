8d75ce24ebd04eeaab2d9533584c9ff1fe32e8b5
TriangularTrike <triangulartrike@web.de>
2021-06-12 12:31:58 +0200

    sprint 2 - BLI 2 - another fix for user controller (login)

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,10 +1,12 @@
 exports.users_login_post = function(req, res) {
-    const doc = req.app.locals.db.getUser(req.query.username);
-    if (doc == null){
-        res.send('No such user');
-    } else if (doc.data().password != req.query.password){
-        res.send('Wrong password entered');
-    } else {
-        res.send('Validation succesful!');
-    }
+    req.app.locals.db.getUser(req.query.username).then((response) => {
+      if (response == null) {
+          res.send('No such user');
+      } else if (response.data().password != req.query.password){
+          res.send('Wrong password entered');
+      } else {
+          res.send('Validation succesful!');
+      }
+    });
+
 };
